4662fa28953522d730199a4f83754bbd18dc15e3
henoas19 <henoas19@b028-23.crio.dauphine.fr>
2019-09-30 15:11:51 +0200

    rajouter un entier

---
+++ td00/src/main/java/fr/dauphine/ja/henouneasma/td00/PrimeCollection.java
@@ -0,0 +1,27 @@
+package fr.dauphine.ja.henouneasma.td00;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrimeCollection 
+{
+	public PrimeCollection{
+		ArrayList numbers = new ArrayList();
+	}
+	
+		initRandom(n,m){
+			
+		}
+	    bool isPrime(int n) {
+	    	if(n<1) 
+	    		return false;
+	    	for(int i=2;i<Math.sqrt(h);i++) {
+	    		if(n%i==0) {
+	    			return false;
+	    		}
+	    	}
+	    	return true;
+	    }
+	
+}
