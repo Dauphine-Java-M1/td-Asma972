0f427ffb171605293d753959da1147eb663c62c5
henoas19 <henoas19@b028-18.crio.dauphine.fr>
2019-10-07 13:40:14 +0200

    ajout d'autres fonctionss

--- td00/src/main/java/fr/dauphine/ja/henouneasma/td00/PrimeCollection.java
+++ td00/src/main/java/fr/dauphine/ja/henouneasma/td00/PrimeCollection.java
@@ -4,24 +4,47 @@ package fr.dauphine.ja.henouneasma.td00;
  * Hello world!
  *
  */
+import java.util.ArrayList;
+import java.util.Random;
+
 public class PrimeCollection 
-{
-	public PrimeCollection{
-		ArrayList numbers = new ArrayList();
-	}
-	
-		initRandom(n,m){
-			
+{	 
+		ArrayList<Integer> numbers; 
+		
+		
+	    public PrimeCollection() {
+			this.numbers=new ArrayList<Integer>();
 		}
-	    bool isPrime(int n) {
-	    	if(n<1) 
-	    		return true;
-	    	for(int i=2;i<Math.sqrt(h);i++) {
-	    		if(n%i==0) {
-	    			return false;
-	    		}
-	    	}
-	    	return true;
+		
+		public void initRandom (int n, int k) {
+			Random r= new Random();
+			for(int i=0;i<n;i++) {
+				numbers.add(r.nextInt(k+1));
+			}
+		}
+		
+		 bool isPrime(int n) {
+		    	if(n<1) 
+		    		return true;
+		    	for(int i=2;i<Math.sqrt(h);i++) {
+		    		if(n%i==0) {
+		    			return false;
+		    		}
+		    	}
+		    	return true;
+		  }
+		 
+		 
+		 public void affichage() {
+				for(Integer i : numbers) {
+					if(isPrime(i)) System.out.println(i);
+				}
+			}
+		
+	    public static void main( String[] args )
+	    {
+	        PrimeCollection l = new PrimeCollection();
+	        l.initRandom(1000,2000);
+	        l.affichage();
 	    }
-	
 }
