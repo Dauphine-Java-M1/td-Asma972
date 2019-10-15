214f1e0ea2dd65e6edcc94614196511a580cd2e9
henoas19 <henoas19@b028-24.crio.dauphine.fr>
2019-10-15 11:01:26 +0200

    exercice 4 -probleme avec equals

--- shapes/src/main/java/fr/dauphine/ja/henouneasma/shapes/Cercle.java
+++ shapes/src/main/java/fr/dauphine/ja/henouneasma/shapes/Cercle.java
@@ -5,9 +5,8 @@ public class Cercle {
 	private Point centre;
 	
 	public Cercle(int rayon, Point p) {
-		this.rayon = rayon;
-		this.centre = new Point(p);
-		
+		this.rayon = rayon; //Rayon du cercle
+		this.centre = new Point(p); //les coordonnée du centre
 	}
 	
 	public int getRayon() {
@@ -17,17 +16,41 @@ public class Cercle {
 	public Point getCentre() {
 		return centre;
 	}
+	
 	@Override
+	//redéfinition de la méthode toString
 	
 	public String toString() {
-	  return (this.getRayon);
+		String res = " rayon= " +this.rayon+" le centre : ("+centre.getX()+","+centre.getY()+")";
+		return res;
+	}
+	// translater un cerlce
+	public void translate(int dx,int dy) {
+		
 	}
 	
-	  public static void main( String[] args )
-	  {
-			Cercle c = new Cercle(1, new Point(1,2));
-			c.getCenter().translate(1,1);
-			System.out.println(c);
-	  }
+	@Override
+	// redéfinition de equals
+	public boolean equals(Circle A){
+		if (centre.getX() = A.getCentre().getX()){
+			if (centre.getY() = A.getCentre().getY(){
+				if (rayon = A.getRayon() ){
+					return true;
+				}
+			}
+		}
+		return false;
+	} 
+	
+	public static void main( String[] args )
+    {
+		Cercle c = new Cercle(1, new Point(1,2));
+		c.getCentre().translate(1,1);
+		Point  p  =	new Point(1,2);
+		Cercle c1 = new Cercle(1,p);
+		Cercle c2 = new Cercle(2,p);
+		c2.translate(1,1);
+		System.out.println(c1+","+c2);
+	}
 
 }
