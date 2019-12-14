ba1b40c5b05cc7d4a7ed84165034385a695bbdb2
Asma972 <henouneasma@yahoo.fr>
2019-12-14 20:59:25 +0100

    exo4 fusion

--- generics/src/main/java/fr/dauphine/ja/henouneasma/generics/GenericsTrois.java
+++ generics/src/main/java/fr/dauphine/ja/henouneasma/generics/GenericsTrois.java
@@ -1,7 +1,10 @@
 package fr.dauphine.ja.henouneasma.generics;
 
+import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 public class GenericsTrois {
@@ -13,18 +16,7 @@ public class GenericsTrois {
 		}
 		return length;
 		}
-	private static List<?  > fusion(List<?> l1, List<?> l2) {
-		// TODO Auto-generated method stub
-		if(l1.size()==l2.size()) {
-			for(int i=0; i<l1.size();i++) {
-				for(int j=0;j<=l2.size();j++){
-					
-				}
-			}
-		}
-		
-		return null;
-	}
+	
 	
 	
 	public static void main(String[] args) {
@@ -32,7 +24,7 @@ public class GenericsTrois {
 
 		List<? extends Integer> ll = Arrays.asList(2,3);
 		System.out.println(listLength(l));
-				List<String> l1= Arrays.asList("C", "rc");
+		List<String> l1= Arrays.asList("C", "rc");
 		List<StringBuilder> l2= Arrays.asList(new StringBuilder("a ma"), new StringBuilder("he!"));
 		//List<? extends CharSequence> r1=fusion(l1,l2);
 		List<?> r2=fusion(l1,l2);
@@ -40,8 +32,22 @@ public class GenericsTrois {
 		List<Integer> l4 = Arrays.asList(10,20);
 		//List<Integer> r3 = fusion(l3,l4);
 		List<?> r4 = fusion(l1,l3);
-
+		System.out.println(r4);
+		System.out.println(r2);
 	}
 
-	
+
+	private static <E> List<E> fusion(List<? extends E> l1, List<? extends E> l2) {
+		// TODO Auto-generated method stub
+		
+		List<Object> es= new ArrayList<Object>();
+		 for( E e: l1){
+			 for( E ee: l2){
+	            es.add(e);
+	            es.add(ee);
+	        }
+		 }
+	        return (List<E>) es;
+	        
+	}
 }
